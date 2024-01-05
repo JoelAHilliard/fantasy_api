5448ec67c7d69e865e8b90f4d986e2586e4d783f
Joel <hilliardjoel05@gmail.com>
2024-01-05 13:14:50 -0800

    fix perfect rsoter

--- src/lts_dashboard.ts
+++ src/lts_dashboard.ts
@@ -1,19 +1,123 @@
-import { privateEncrypt } from "crypto";
 import { MongoClient } from "mongodb";
 
 ///used for caching
 let league_data:any = {};
+//ts-nocheck
+function generateBestRoster(boxScores:any) {
+    const perfectRoster = {
+        "QB": [],
+        "RB": [],
+        "WR": [],
+        "TE": [],
+        "FLEX": [],
+        "D/ST": [],
+        "K": []
+    };
+
+    let flexCandidates = [];
+
+    for (const boxScore of boxScores) {
+        for (let player of (boxScore.home_team_lineup || [])) {
+            player.owner_team_name = boxScore.home_team;  // Assign home team name
+    
+            // Process the player (example for QB, you can add more positions)
+            switch (player.position) {
+                case "QB":
+                    if (!perfectRoster.QB[0] || perfectRoster.QB[0].points < player.points) {
+                        perfectRoster.QB = [player]
+                    }
+                    break;
+                case "RB":
+                    perfectRoster.RB.push(player);
+                    break;
+                case "WR":
+                    perfectRoster.WR.push(player);
+                    break;
+                case "TE":
+                    if (!perfectRoster.TE[0] || perfectRoster.TE[0].points < player.points) {
+                        perfectRoster.TE = [player];
+                    }
+                    break;
+                case "D/ST":
+                    if (!perfectRoster["D/ST"][0] || perfectRoster["D/ST"][0].points < player.points) {
+                        perfectRoster["D/ST"] = [player];
+                    }
+                    break;
+                case "K":
+                    if (!perfectRoster.K[0] || perfectRoster.K[0].points < player.points) {
+                        perfectRoster.K = [player];
+                    }
+                    break;
+                default:
+                    flexCandidates.push(player);
+            }
+        }
+        for (let player of (boxScore.away_team_lineup || [])) {
+            player.owner_team_name = boxScore.away_team;  // Assign home team name
+    
+            // Process the player (example for QB, you can add more positions)
+            switch (player.position) {
+                case "QB":
+                    if (!perfectRoster.QB[0] || perfectRoster.QB[0].points < player.points) {
+                        perfectRoster.QB = [player]
+                    }
+                    break;
+                case "RB":
+                    perfectRoster.RB.push(player);
+                    break;
+                case "WR":
+                    perfectRoster.WR.push(player);
+                    break;
+                case "TE":
+                    if (!perfectRoster.TE[0] || perfectRoster.TE[0].points < player.points) {
+                        perfectRoster.TE = [player];
+                    }
+                    break;
+                case "D/ST":
+                    if (!perfectRoster["D/ST"][0] || perfectRoster["D/ST"][0].points < player.points) {
+                        perfectRoster["D/ST"] = [player];
+                    }
+                    break;
+                case "K":
+                    if (!perfectRoster.K[0] || perfectRoster.K[0].points < player.points) {
+                        perfectRoster.K = [player];
+                    }
+                    break;
+                default:
+                    flexCandidates.push(player);
+            }
+        }
+    }
+
+    perfectRoster.RB.sort((a, b) => b.points - a.points);
+    perfectRoster.WR.sort((a, b) => b.points - a.points);
+    flexCandidates.push(...perfectRoster.RB.slice(2), ...perfectRoster.WR.slice(2));
+    if (perfectRoster.TE) flexCandidates.push(perfectRoster.TE);
+    flexCandidates.sort((a, b) => b.points - a.points);
+
+    perfectRoster.RB = perfectRoster.RB.slice(0, 2);
+    perfectRoster.WR = perfectRoster.WR.slice(0, 2);
+    perfectRoster.FLEX = [flexCandidates[0]];
+
+    return perfectRoster;
+}
+
+
 
 async function getLTS(client:MongoClient,refresh:boolean,params:any)
 {
     const LEAGUEID = Number(params.league_id);
+    
     let WEEK = Number(params.week);
+    
     if(Number.isNaN(LEAGUEID)){
         return {"error":"Must pass League_ID"}
     }
+    
     if(Number.isNaN(WEEK)){
         WEEK = 17
     }
+    
     console.log('/lts | league_id:' + params.league_id)
 
     if(league_data["league_"+String(LEAGUEID)+"_"+String(WEEK)])
@@ -47,11 +151,12 @@ async function getLTS(client:MongoClient,refresh:boolean,params:any)
     
     let matchups: any = [];
 
-    let standings: any = league_info_data[0].standings
+    let standings: any = league_info_data[0].standings;
     
-    let power_rankings: any = league_info_data[0].pwrRankings
-    //left off here
-    let perfect_roster = league_info_data[0].top_roster;
+    let power_rankings: any = league_info_data[0].pwrRankings;
+    
+    //left off here - generate perf roster here
+    let perfect_roster = generateBestRoster(matchups_data);
 
     for (let item in matchups_data)
     {
@@ -64,7 +169,7 @@ async function getLTS(client:MongoClient,refresh:boolean,params:any)
     delete league_info_data[0].pwrRankings
     delete league_info_data[0].top_roster
 
-    perfect_roster["FLEX"] = [perfect_roster["FLEX"]]
+    // perfect_roster["FLEX"] = [perfect_roster["FLEX"]]
 
     const res = {
         "matchups": matchups,
